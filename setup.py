d17f14153e3e4e8c40de0d7273c59ca6ec1109f2
Sergey Levitin <sergey.levitin@webpp.ru>
2015-10-01 17:30:28 +0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='codestyle',
-    version='0.0.5',
+    version='0.0.6',
     author=u'Sergey Levitin',
     author_email='selevit@gmail.com',
     packages=['codestyle'],
