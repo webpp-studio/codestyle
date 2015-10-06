c4bd9afaf123d0e376388bf6d57eb85b6aabe3cb
Sergey Levitin <sergey.levitin@webpp.ru>
2015-10-06 15:25:16 +0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codestyle',
-    version='0.0.20',
+    version='0.0.21',
     author=u'Sergey Levitin',
     author_email='selevit@gmail.com',
     packages=find_packages(),
