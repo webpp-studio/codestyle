f755c6c23aafa25f287fcaae4dcc29d2d7cd703e
Sergey Levitin <sergey.levitin@webpp.ru>
2015-10-07 16:45:32 +0300

    Change PHP code style

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codestyle',
-    version='0.0.24',
+    version='0.0.25',
     author=u'Sergey Levitin',
     author_email='selevit@gmail.com',
     packages=find_packages(),
