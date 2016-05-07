2d70befd4323d56abcfbc13d69ee759b0239cfef
Sergey Levitin <selevit@gmail.com>
2016-05-07 11:16:07 +0300

    Fix dependency conflicts. Update CI script.

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@ FROM ubuntu:14.04
 
 RUN apt-get update -qq && apt-get install -yq \
     python-pip nodejs npm php-pear
-RUN pip install codestyle
 RUN npm install -g jshint jscs jscs-fixer csscomb htmlcs walk brace-expansion
 RUN pear install PHP_CodeSniffer
 RUN ln -s /usr/bin/nodejs /usr/bin/node
+COPY . /tmp/codestyle
+RUN cd /tmp/codestyle && python setup.py install
+RUN rm -rf /tmp/codestyle
 
 ENTRYPOINT ["codestyle"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codestyle',
-    version='0.0.30',
+    version='0.0.31',
     author=u'Sergey Levitin',
     author_email='selevit@gmail.com',
     packages=find_packages(),
@@ -19,6 +19,12 @@ setup(
     description='Extendable codestyle checker and fixer',
     long_description=open('README.rst').read(),
     scripts=['scripts/codestyle'],
-    install_requires=['pep8', 'autopep8', 'flake8', 'autoflake'],
+    install_requires=[
+        'pep8>=1.5.7,<1.6',
+        'pyflakes>=0.8.1,<0.9',
+        'autopep8>=1.2,<=1.2.99',
+        'flake8>=2.4,<=2.4.99',
+        'autoflake>=0.6,<=0.6.99'
+    ],
     test_suite='tests'
 )
