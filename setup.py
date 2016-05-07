8c187111eb7f6b61166fd3402e21330a67dc07a5
Sergey Levitin <selevit@gmail.com>
2016-05-07 11:36:38 +0300

    Change default rules for HTML

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codestyle',
-    version='0.0.31',
+    version='0.0.32',
     author=u'Sergey Levitin',
     author_email='selevit@gmail.com',
     packages=find_packages(),
