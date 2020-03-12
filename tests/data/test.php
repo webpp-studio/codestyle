08ab6823102156a798588cb6eadc51dd66b7172b
Антон Седов <anton.sedov@webpp.ru>
2020-03-12 11:01:28 +0400

    Добавление обязательности phpdocs для функций и методов. Разрешение на перенос операторов на новую строку.

--- tests/data/test.php
+++ tests/data/test.php
@@ -1,28 +1,61 @@
 <?php
 
-function testMyFunc($arg1, $arg2)
+namespace foo;
+
+/**
+ * Class Vendor\Foo
+ * @tag tag
+ */
+class FooTest
+{
+
+}
+
+/**
+ * Foo docks
+ * @param int $arg1 arg1 comment
+ * @param int $arg2 arg2 comment
+ * @return float
+ */
+function testFunc(int $arg1, int $arg2)
 {
     $var = $arg1 + $arg2;
 
     return $var * 0.1;
 }
 
-
-function testMyFunc($arg1, $agg)
+/**
+ * Foo docks
+ *
+ * @param int $arg1 comment
+ * @param int $agg comment
+ * @return float
+ */
+function testMyFunc(int $arg1, int $agg): float
 {
     $var = $arg1 + $agg;
 
-    return $var / 20;
+    return (float) $var / 20;
 }
 
+/**
+ * Foo docks
+ *
+ * @return string
+ */
 function test()
 {
     return "тестовая длинная строка не-ansi символов для проверки длины";
 }
 
+/**
+ * @param int $param1 comment
+ * @param int $param2 comment
+ * @return int
+ */
 function testMultiLineFunc(
-    int param1,
-    int param2
+    int $param1,
+    int $param2
 ): int {
-    return param1 * param2;
+    return $param1 * $param2;
 }
